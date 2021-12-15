dd2f5717680267ec35a3bb956c89910306a1edd4
Evgeniy585 <ev.zhuravlev95@mail.ru>
2021-12-15 20:29:10 +0300

    Написал код,который переводит евро в рубли

---
+++ exercise 24.php
@@ -0,0 +1,10 @@
+<?php
+
+$eurosCount = 100;
+// BEGIN (write your solution here)
+$dollarsCount = $eurosCount * 1.25;
+print_r($dollarsCount);
+print_r("\n");
+$rublesCount = $dollarsCount * 60;
+print_r($rublesCount);
+// END
