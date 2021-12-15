0e8a5974f415756bdfaffd103be22cf74457728c
Evgeniy585 <ev.zhuravlev95@mail.ru>
2021-12-15 22:09:35 +0300

    Дополнил код, создав новые переменные

---
+++ exercise 27.php
@@ -0,0 +1,9 @@
+<?php
+
+$king = 'King Balon the 6th';
+
+// BEGIN (write your solution here)
+$castlesCount = 6;
+$roomsInCastleCount = 17;
+print_r($king . ' has ' . ($castlesCount * $roomsInCastleCount) . ' rooms.');
+// END
