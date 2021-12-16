77d5216fe0cffabdc637b04ff3093b35dd7d7457
Evgeniy585 <ev.zhuravlev95@mail.ru>
2021-12-16 14:09:15 +0300

    Вывел на экран расстояние между городами

---
+++ exercise 33.php
@@ -0,0 +1,8 @@
+<?php
+
+use function HexletBasics\Functions\calculateDistance;
+
+// BEGIN (write your solution here)
+$length = calculateDistance ('Qarth', 'Vaes Dothrak');
+print_r($length);
+// END
