e036afe4d3e5df6e2d85d38098ca1409ef80e7d9
Evgeniy585 <ev.zhuravlev95@mail.ru>
2021-12-20 21:38:58 +0300

    Написал функцию, которая выводит на экран повторяющиеся значения

---
+++ exercise 45.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HexletBasics\DefineFunctions\Arguments;
+
+// BEGIN (write your solution here)
+function printSeq($string, $number)
+{
+    print_r(str_repeat($string, $number));
+}
+// END
