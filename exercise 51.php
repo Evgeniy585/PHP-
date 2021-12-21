4632863e152691d7cc7024a7332bbdc4d30f4219
Evgeniy585 <ev.zhuravlev95@mail.ru>
2021-12-21 11:28:28 +0300

    Реализовал функцию, которая округляет число в нижнюю сторону

---
+++ exercise 51.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HexletBasics\DefineFunctions\ReadingDocumentation;
+
+// BEGIN (write your solution here)
+function getAge($val)
+{
+    return round($val, 0, PHP_ROUND_HALF_DOWN);
+}
+// END
