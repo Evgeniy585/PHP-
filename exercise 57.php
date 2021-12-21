9faeac3d5cb3a671a096d6ac5c6d5e9ad6483667
Evgeniy585 <ev.zhuravlev95@mail.ru>
2021-12-21 21:27:38 +0300

    Написал функцию с логическими операторами

---
+++ exercise 57.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HexletBasics\Logic\LogicalOperators2;
+
+// BEGIN (write your solution here)
+function isNeutralSoldier($armor, $shield)
+{
+return($armor !== 'red' && $shield === 'black');
+}
+// END
