f486e9d75afa639c8fa59be271a16b0e340fc7c6
Evgeniy585 <ev.zhuravlev95@mail.ru>
2022-01-02 00:27:34 +0300

    Написал функцию, используя, так называемый, Синтаксический сахар

---
+++ exercise 72.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace HexletBasics\Loops\SyntaxSugar;
+
+// BEGIN (write your solution here)
+function countChars($str, $char)
+{
+    $i = 0;
+    $count = 0;
+    while ($i < strlen($str)) {
+        if ($str[$i] === $char) {
+            $count = $count + 1;
+        }
+         $i += 1;
+    }
+
+    return $count;
+}
+// END
